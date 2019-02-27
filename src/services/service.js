bb6b7466d33f50c0310919d78c91d13a141cff6b
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-02-27 00:45:04 -0500

    Services file

--- src/services/service.js
+++ src/services/service.js
@@ -1,7 +1,7 @@
-import Axios from 'axios';
+import axios from 'axios';
 
-const getVideos = (search) => {
-  Axios({
+const getSearchResults = (search, token) => {
+  return axios({
       method: 'get',
       url: 'https://www.googleapis.com/youtube/v3/search',
       params: {
@@ -12,33 +12,33 @@ const getVideos = (search) => {
         videoEmbeddable: 'true',
         key: 'AIzaSyCW33eITeJJ3jx8nVJKFylDq6R4V5aTWGU',
         q: `${search}`,
-        pageToken: ''
+        pageToken: `${token}`
       }
     })
     .then((res)=>{
       return res.data.items.map((e,i)=>{
         let video = {
           'vidID': e.id.videoId,
-          'title':e.snippet.title,
+          'title': e.snippet.title,
           'channelTitle': e.snippet.channelTitle,
           'description': e.snippet.description,
-          'publishedAt' :e.snippet.publishedAt,
-          'thumbnail' : `https://i.ytimg.com/vi/${e.id.videoId}/maxresdefault.jpg`
+          'publishedAt': e.snippet.publishedAt,
+          'thumbnail' : `https://i.ytimg.com/vi/${e.id.videoId}/mqdefault.jpg`,
+          'token' : res.data.nextPageToken
         };
         return video;
       })
-    }).then((video)=>{
-      console.log(video)
     })
 }
 
 //Grab In
-const getInfo = (videoID) => {
-  Axios({
+const getVideoInfo = (videoID) => {
+  return axios({
       method: 'get',
       url: 'https://www.googleapis.com/youtube/v3/videos',
       params: {
         id: `${videoID}`,
+        type: 'list',
         part: 'snippet,contentDetails,statistics',
         key: 'AIzaSyCW33eITeJJ3jx8nVJKFylDq6R4V5aTWGU',
       }
@@ -50,19 +50,53 @@ const getInfo = (videoID) => {
         'duration' : info.contentDetails.duration,
         'channelTitle': info.snippet.channelTitle,
         'description' : info.snippet.description,
-        'viewCount':info.statistics.viewCount,
-        'publishedAt' :info.snippet.publishedAt,
+        'viewCount': info.statistics.viewCount,
+        'publishedAt' : info.snippet.publishedAt,
         'thumbnail': info.snippet.thumbnails.maxres.url,
       };
       return videoStats;
     })
-    .then((videoStats)=>{
-      console.log(videoStats)
-    })
 }
 
-export {getVideos, getInfo}
+const getItem = (keyName) => {
+  return new Promise ((resolve, reject) => {
+    if(typeof keyName === "string"){
+      if(localStorage.getItem(keyName)){
+        resolve(localStorage.getItem(keyName));
+      }
+      else{
+        reject('Invalid data, key does not exist')
+      }
+    }
+    else{
+      reject('Invalid data, keyName must be a string')
+    }
+  })
+  .then(data => {
+    return JSON.parse(data);
+  })
+  .catch(err => {
+    console.log('getItem: ',err)
+  })
+};
 
-// localStorage.getItem('nameofKey') //users, recentUsers
-// localStorage.setItem('nameofKey','value')
-// JSON.stringify()
+const setItem = (keyName, keyValue) => {
+  return new Promise ((resolve, reject) => {    
+    if(typeof keyName === "string"){
+      const data = {'keyName':keyName, 'keyValue':keyValue}
+      resolve(data)
+    }
+    else{
+      reject('Invalid data, keyName must be a string')
+    }
+  })
+  .then(data => {
+    if(typeof data.keyValue === 'string') localStorage.setItem(`${data.keyName}`,`${data.keyValue}`)
+    else localStorage.setItem(`${data.keyName}`, JSON.stringify(data.keyValue))
+  })
+  .catch(err => {
+    console.log('setItem: ',err)
+  })
+}
+
+export {getSearchResults, getVideoInfo, setItem, getItem}
