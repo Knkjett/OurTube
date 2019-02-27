2bc2a0deb4055bee8779de48c814cd004bda11e8
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-02-27 00:49:30 -0500

    Services file finalized with token and proper query to be used

--- src/services/service.js
+++ src/services/service.js
@@ -1,3 +1,102 @@
-localStorage.getItem('nameofKey') //users, recentUsers
-localStorage.setItem('nameofKey','value')
-JSON.stringify()
+import axios from 'axios';
+
+const getSearchResults = (search, token) => {
+  return axios({
+      method: 'get',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        part: 'snippet',
+        maxResults: 24,
+        videoDefinition: 'high',
+        type: 'video',
+        videoEmbeddable: 'true',
+        key: 'AIzaSyCW33eITeJJ3jx8nVJKFylDq6R4V5aTWGU',
+        q: `${search}`,
+        pageToken: `${token}`
+      }
+    })
+    .then((res)=>{
+      return res.data.items.map((e,i)=>{
+        let video = {
+          'vidID': e.id.videoId,
+          'title': e.snippet.title,
+          'channelTitle': e.snippet.channelTitle,
+          'description': e.snippet.description,
+          'publishedAt': e.snippet.publishedAt,
+          'thumbnail' : `https://i.ytimg.com/vi/${e.id.videoId}/mqdefault.jpg`,
+          'token' : res.data.nextPageToken
+        };
+        return video;
+      })
+    })
+}
+
+//Grab In
+const getVideoInfo = (videoID) => {
+  return axios({
+      method: 'get',
+      url: 'https://www.googleapis.com/youtube/v3/videos',
+      params: {
+        id: `${videoID}`,
+        type: 'list',
+        part: 'snippet,contentDetails,statistics',
+        key: 'AIzaSyCW33eITeJJ3jx8nVJKFylDq6R4V5aTWGU',
+      }
+    })
+    .then((res)=>{
+      let info = res.data.items[0];
+      let videoStats = {
+        'title': info.snippet.title,
+        'duration' : info.contentDetails.duration,
+        'channelTitle': info.snippet.channelTitle,
+        'description' : info.snippet.description,
+        'viewCount': info.statistics.viewCount,
+        'publishedAt' : info.snippet.publishedAt,
+        'thumbnail': info.snippet.thumbnails.maxres.url,
+      };
+      return videoStats;
+    })
+}
+
+const getItem = (keyName) => {
+  return new Promise ((resolve, reject) => {
+    if(typeof keyName === "string"){
+      if(localStorage.getItem(keyName)){
+        resolve(localStorage.getItem(keyName));
+      }
+      else{
+        reject('Invalid data, key does not exist')
+      }
+    }
+    else{
+      reject('Invalid data, keyName must be a string')
+    }
+  })
+  .then(data => {
+    return JSON.parse(data);
+  })
+  .catch(err => {
+    console.log('getItem: ',err)
+  })
+};
+
+const setItem = (keyName, keyValue) => {
+  return new Promise ((resolve, reject) => {    
+    if(typeof keyName === "string"){
+      const data = {'keyName':keyName, 'keyValue':keyValue}
+      resolve(data)
+    }
+    else{
+      reject('Invalid data, keyName must be a string')
+    }
+  })
+  .then(data => {
+    if(typeof data.keyValue === 'string') localStorage.setItem(`${data.keyName}`,`${data.keyValue}`)
+    else localStorage.setItem(`${data.keyName}`, JSON.stringify(data.keyValue))
+  })
+  .catch(err => {
+    console.log('setItem: ',err)
+  })
+}
+
+export {getSearchResults, getVideoInfo, setItem, getItem}
